6263f3d037acee804d9b7c07404ffdc17a2f4c7b
Lohithroshan45 <lohithroshan.23@ichat.sp.edu.sg>
2026-01-02 17:00:17 +0800

    changes in buttons(react-website)

--- react-website/src/components/HeroSection.js
+++ react-website/src/components/HeroSection.js
@@ -7,15 +7,17 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src="/videos/nature.mp4" autoPlay loop muted />
-      <h1> ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1> SUSTAINABILITY</h1>
+      <p>What</p>
       <div className="hero-btns">
         <Button className='btns' buttonStyle='btn--outline'
-        buttonSize='btn--large'>
+        buttonSize='btn--large'
+        >
             GET STARTED
             </Button>
-            <Button className='btns' buttonStyle='btn--primary'
-        buttonSize='btn--large'>
+            <Button className='btns' 
+            buttonStyle='btn--primary' 
+            buttonSize='btn--large'>
             WATCH TRAILER <i className='far fa-play-circle' />
             </Button>
       </div>
